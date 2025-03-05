b7230cd4696d807be0e9b8e713090253149256e1
Aditya Pandey <adityapandey@Adityas-MacBook-Air.local>
2025-03-05 16:32:43 +0530

    deployment

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
+
 use common::agg_mod;
 //use clap::{Parser, Subcommand};
 // use game::GameManager;
 use dotenv::dotenv;
 use game::GameServer;
+use tracing::info;
 
 //mod macros;
 
@@ -26,26 +29,26 @@ agg_mod!(board game player seed_gen);
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // Load environment variables from .env file if it exists
     dotenv().ok();
-    //println!("Starting the server");
-    //let cli = Cli::parse();
-    //
-    //match cli.command {
-    //    Commands::Server => {
-    //        let game_server = GameServer::new();
-    //
-    //        game_server.start("127.0.0.1:3000").await?;
-    //    }
-    //    // Commands::Client => {
-    //    //     let client = GameClient::new("ws://127.0.0.1:3000").await?;
-    //    //     client.run_client().await?;
-    //    // }
-    //    _ => {}
-    //}
-    //
+    // Set the default log level to info
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO) // Set the log level to INFO
+        .init();
+    info!("Starting the game server");
+
+    // // Environment variables are already set in docker-compose.yml
+    // // We can still try to load from .env files as fallback for local development
+    // let env = env::var("APP_ENV").unwrap_or_else(|_| "dev".to_string());
+    // if env == "dev" || env == "local" {
+    //     let env_file = format!(".env.{}", env);
+    //     dotenv::from_filename(env_file).ok();
+    // }
+
+    // info!("Starting the deposit background service");
 
     let game_server = GameServer::new().await;
-    game_server.start("127.0.0.1:3000").await?;
+    game_server.start("0.0.0.0:3000").await?;
 
     Ok(())
 }
