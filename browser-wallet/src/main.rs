4ba94483b404eb0fb45821bfe99fbb1e4c455215
Aditya Pandey <adityapandey@Adityas-MacBook-Air.local>
2025-04-30 11:46:29 +0530

    some renaming

--- browser-wallet/src/main.rs
+++ browser-wallet/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use common::{
@@ -226,17 +228,19 @@ async fn deposit(
 
     tx.commit().await.expect("Failed to commit transaction");
 
-    // Send Telegram notification about the deposit
-    let message = format!(
-        "💰 New Deposit!\nUser ID: {}\nAmount: {} {:?}\nTransaction Hash: {}",
-        deposit_request.user_id,
-        deposit_request.amount,
-        deposit_request.currency,
-        deposit_request.tx_hash
-    );
+    if env::var("PROFILE").unwrap_or_else(|_| "prod".to_string()) == "prod" {
+        // Send Telegram notification about the deposit
+        let message = format!(
+            "💰 New Deposit!\nUser ID: {}\nAmount: {} {:?}\nTransaction Hash: {}",
+            deposit_request.user_id,
+            deposit_request.amount,
+            deposit_request.currency,
+            deposit_request.tx_hash
+        );
 
-    if let Err(e) = telegram::send_telegram_message(&message).await {
-        error!("Failed to send Telegram notification: {}", e);
+        if let Err(e) = telegram::send_telegram_message(&message).await {
+            error!("Failed to send Telegram notification: {}", e);
+        }
     }
 
     HttpResponse::Ok().json(json!({
